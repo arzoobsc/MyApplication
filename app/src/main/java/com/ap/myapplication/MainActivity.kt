a88e0b74f2623e69bec4bdaffe21321caaf5194f
arzoobsc <arzoobsc@gmail.com>
2020-05-31 09:29:41 +0530

    Toast set in TextView

--- app/src/main/java/com/ap/myapplication/MainActivity.kt
+++ app/src/main/java/com/ap/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ap.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv1.setOnClickListener {
+            Toast.makeText(this, "tv Clicked...", Toast.LENGTH_LONG)
+        }
     }
 }
